b92911dc066da8990f026629468ff3c01c95d0ac
H3R01A <aallen0001@gmail.com>
2024-03-30 12:25:31 -0700

    add Big O explanation and refactor for most optimal version

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -20,22 +20,36 @@
 
 function bubbleSort(nums) {
   //create a swapped flag
-  let swapped = true;
-  //check to determine if there was a swap? If there was, means additional sorting will need to be done
-  while (swapped) {
+  let swapped = false;
+  //create tracker for num of times we moved large nums to correct spot
+  let numSwapped = 0;
+  // Do While Loop (will happen at least once)
+  do {
     //make swapped flag false. If there is a swap, this will change to true
     swapped = false;
     //loop to check neighbor numbers
-    for (let i = 0; i < nums.length; i++) {
+    //loop until corrected spots where the largest nums are at. No need to go further
+    for (let i = 0; i < nums.length - numSwapped - 1; i++) {
+
       //check if current num > next num if so swap. If not, do nothing
       if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+
         //swapping using Destructuring
-        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+        //! What is the time complexity of this? One post says O(n)...
+        // [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+
         swapped = true;
       }
     }
-  }
-  //finally return sorted arr
+
+    //if we successfully swapped, then increment tracker
+    if (swapped) numSwapped++;
+
+  } while (swapped);
+
   return nums;
 }
 
@@ -43,6 +57,35 @@ function bubbleSort(nums) {
 //[1, 5, 4, 2, 3] > [1, 2, 3, 4, 5]
 //[2,4,1] > [1,2,4]
 //[0,0,0] > [0,0,0]
+
+// const arr = [1, 5, 4, 2, 3];
+
+// console.log(bubbleSort(arr));
+
+//Big O Explanations
+
+/**
+ * Time Complexity
+ * O(n2) - because of the outer while loop (n) and the inner for loop (n)
+ * while loop - will keep looping if there were unsorted elements
+ * for loop - will iterate through the number of elements in the passed in array
+ *
+ *
+ * Space Complexity
+ * O(1) because we are not creating any new places in memory (i.e. arrays)
+ *
+ *
+ *
+ *
+ * Optimal solution
+ *  Time Complexity
+ *  O(n) - However in order to get an optimal solution, a 'sorted' array would need to be passed in the function
+ * 
+ *  Space Complexity
+ *  O(1) - remains the same. Nothing has changed.
+ *  
+ *
+ */
 
 // unit tests
 // do not modify the below code
