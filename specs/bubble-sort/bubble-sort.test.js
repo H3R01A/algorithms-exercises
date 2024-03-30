5ece4044b55a90baca570db5454c47e22cea527d
H3R01A <aallen0001@gmail.com>
2024-03-30 10:23:16 -0700

    solved bubble-sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,13 +10,43 @@
   index's. Continue looping through until all values are in ascending order
 */
 
+//i: array of numbers
+//o: sorted array
+
+//test cases
+//[1, 5, 4, 2, 3] > [1, 2, 3, 4, 5]
+//[2,4,1] > [1,2,4]
+//[0,0,0] > [0,0,0]
+
 function bubbleSort(nums) {
-  // code goes here
+  //create a swapped flag
+  let swapped = true;
+  //check to determine if there was a swap? If there was, means additional sorting will need to be done
+  while (swapped) {
+    //make swapped flag false. If there is a swap, this will change to true
+    swapped = false;
+    //loop to check neighbor numbers
+    for (let i = 0; i < nums.length; i++) {
+      //check if current num > next num if so swap. If not, do nothing
+      if (nums[i] > nums[i + 1]) {
+        //swapping using Destructuring
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+        swapped = true;
+      }
+    }
+  }
+  //finally return sorted arr
+  return nums;
 }
+
+//test cases
+//[1, 5, 4, 2, 3] > [1, 2, 3, 4, 5]
+//[2,4,1] > [1,2,4]
+//[0,0,0] > [0,0,0]
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test('bubble sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
